7e07feb7081e3bdea02a4a4d0ae1b20aba361686
Swati <83855603+SwatiAF@users.noreply.github.com>
2022-01-20 22:05:00 +0530

    example js validation

--- Form Validation/form valid.js
+++ Form Validation/form valid.js
@@ -1,9 +1,15 @@
-const email = document.getElementById("mail");
+function validateform() {
+  var name = document.myform.name.value;
+  var password = document.myform.password.value;
 
-email.addEventListener("input", function (event) {
-  if (email.validity.typeMismatch) {
-    email.setCustomValidity("I am expecting an e-mail address!");
-  } else {
-    email.setCustomValidity("");
+  if (name == null || name == "") {
+    alert("Name can't be blank");
+    return false;
+  } else if (password.length < 6) {
+    alert("Password must be at least 6 characters long.");
+    return false;
+  } else if (password == "password") {
+    alert("This password is invalid");
+    return false;
   }
-});
+}
