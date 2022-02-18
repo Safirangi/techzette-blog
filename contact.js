fd702b2007c34f5f099a6032a9f5a210628b5a56
Swati <83855603+SwatiAF@users.noreply.github.com>
2022-02-18 12:17:53 +0530

    altered alert msg for invalid email

--- contact.js
+++ contact.js
@@ -39,11 +39,13 @@ function contact() {
 //Checks for valid email address format
 function emailValidate(inputText) {
   var mailformat = /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/;
+  var email = document.form.email.value;
+  
   if (inputText.value.match(mailformat)) {
     alert("Valid email address!");
     document.form1.text1.focus();
     return true;
-  } else {
+  } else if (email != ""){
     alert("You have entered an invalid email address!");
     document.form1.text1.focus();
     return false;
